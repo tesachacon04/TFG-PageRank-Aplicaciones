830776f4e8ae1ea848f15f426e9f4b1ecf397be8
tesachacon04 <tesachagil04@gmail.com>
2026-04-06 16:51:25 +0200

    Add files via upload

---
+++ grafoLLAM.m
@@ -0,0 +1,6 @@
+
+MiGrafo2 = digraph(LLAM); 
+p2 = plot(MiGrafo2);
+p2.NodeColor = 'r';      % Nodos en rojo
+p2.EdgeColor = 'k';      % Líneas en negro
+p2.MarkerSize = 6;       % Tamaño de los nodos
---
+++ grafoPPAM.m
@@ -0,0 +1,6 @@
+
+MiGrafo1 = digraph(PPAM); 
+p1 = plot(MiGrafo);
+p1.NodeColor = 'r';      % Nodos en rojo
+p1.EdgeColor = 'k';      % Líneas en negro
+p1.MarkerSize = 6;       % Tamaño de los nodos
