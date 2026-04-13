37e6ab180149cf7127229199a317a2c4ece7f955
tesachacon04 <tesachagil04@gmail.com>
2026-04-13 18:22:21 +0200

    Delete grafoLLAM.m

--- grafoLLAM.m
+++
@@ -1,6 +0,0 @@
-
-MiGrafo2 = digraph(LLAM); 
-p2 = plot(MiGrafo2);
-p2.NodeColor = 'r';      % Nodos en rojo
-p2.EdgeColor = 'k';      % Líneas en negro
-p2.MarkerSize = 6;       % Tamaño de los nodos
